b8aeb9270454cc4a58444e00010fcda7a491ab03
Matthew J. Sahagian <matthew.sahagian@gmail.com>
2025-03-23 08:30:25 -0700

    Get hacky for HTMX

--- src/Resources/views/layout.php
+++ src/Resources/views/layout.php
@@ -6,23 +6,23 @@
  */
 ?>
 <!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?= $this->escape($pageTitle); ?></title>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title><?= $this->escape($pageTitle); ?></title>
+	</head>
+	<body style="width: 100%; height: auto; margin: 0; padding: 0;">
+		<?= $resourceLoader->stylesheet("bootstrap.css"); ?>
+		<?= $resourceLoader->stylesheet("fontello.css"); ?>
+		<?= $resourceLoader->stylesheet("main.css"); ?>
+		<?= $resourceLoader->stylesheet("themes/default.css"); ?>
 
-    <?= $resourceLoader->stylesheet("bootstrap.css"); ?>
-    <?= $resourceLoader->stylesheet("fontello.css"); ?>
-    <?= $resourceLoader->stylesheet("main.css"); ?>
-    <?= $resourceLoader->stylesheet("themes/default.css"); ?>
-</head>
-<body>
-<div id="page">
-    <?= $this->section("content"); ?>
-</div>
-<?= $resourceLoader->script("prettify.js"); ?>
-<?= $resourceLoader->script("main.js"); ?>
-</body>
+		<div id="page">
+			<?= $this->section("content"); ?>
+		</div>
+		<?= $resourceLoader->script("prettify.js"); ?>
+		<?= $resourceLoader->script("main.js"); ?>
+	</body>
 </html>
